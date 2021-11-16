8a846b46a14a7bb2735be63086b5d9b1fa007fa3
PatrickBarsch <PatrickGB@gmx.de>
2021-11-16 16:38:49 +0100

    Thermostat has a down function to decrease temperature

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,9 +1,21 @@
+'use strict';
+
 describe('thermostat', function(){
+  let thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
+  
   it('has a default temperature of 20', function() {
     expect(thermostat.temperature).toEqual(20)
   });
   it('has an up function to increase temperature', function() {
     thermostat.up()
     expect(thermostat.temperature).toEqual(21)
+  });
+  it('has a down fuction to decrease the temperature', function() {
+    thermostat.down()
+    expect(thermostat.temperature).toEqual(19)
   });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,6 +1,9 @@
-const thermostat = {
-  temperature: 20,
-  up() {
-    this.temperature++
+'use strict';
+
+class Thermostat {
+  constructor() { 
+    this.temperature = 20 
   }
+  up() { this.temperature++ }
+  down() { this.temperature-- }
 }
